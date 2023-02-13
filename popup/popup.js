5174aee5c50c2a5e3a71821493a0ba9889181764
Ankur Solanki <ankursolanki1537@gmail.com>
2023-02-13 23:24:12 +0530

    different page styling change issue

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,9 @@
-const button = document.getElementById("invokebutton")
+const latestButton = document.getElementById("invokebutton")
+const airingbutton = document.getElementById("airingbutton")
+const popularbutton = document.getElementById("popularbutton")
 const loading = document.getElementById("loading")
-console.log(button)
 
-button.addEventListener("click",async ()=>{
+latestButton.addEventListener("click",async ()=>{
     loading.style.display = "block"
 
     console.log("blickd")
@@ -17,8 +18,52 @@ button.addEventListener("click",async ()=>{
     const response = await data.json()
 
     const [tab] = await chrome.tabs.query({active:true, currentWindow: true})
-    console.log(tab)
-    const res = await chrome.tabs.sendMessage(tab.id,{response})
+    const dataToSend = {buttonColor:getComputedStyle(latestButton).backgroundColor,
+        buttonName:"Latest Episodes",response:response}
+    const res = await chrome.tabs.sendMessage(tab.id,dataToSend)
+    loading.style.display = "none"
+    console.log(res)
+    console.log(response)
+})
+
+airingbutton.addEventListener("click",async ()=>{
+    loading.style.display = "block"
+
+    console.log("blickd")
+    const data = await fetch('https://gogoanime.consumet.stream/top-airing',{
+        method:"GET",
+        headers:{
+            'X-RapidAPI-Key': '2f29f442ccmshbfca029a7fa434dp180a15jsn270ca0d138c5',
+		    'X-RapidAPI-Host': 'gogoanime2.p.rapidapi.com'
+        }
+    })
+    
+    const response = await data.json()
+    const [tab] = await chrome.tabs.query({active:true, currentWindow: true})
+    const dataToSend = {buttonColor:getComputedStyle(airingbutton).backgroundColor,
+    buttonName:"Top Airing",response:response}
+    const res = await chrome.tabs.sendMessage(tab.id,dataToSend)
+    loading.style.display = "none"
+})
+
+popularbutton.addEventListener("click",async ()=>{
+    loading.style.display = "block"
+
+    console.log("blickd")
+    const data = await fetch('https://gogoanime2.p.rapidapi.com/popular',{
+        method:"GET",
+        headers:{
+            'X-RapidAPI-Key': '2f29f442ccmshbfca029a7fa434dp180a15jsn270ca0d138c5',
+		    'X-RapidAPI-Host': 'gogoanime2.p.rapidapi.com'
+        }
+    })
+    
+    const response = await data.json()
+
+    const [tab] = await chrome.tabs.query({active:true, currentWindow: true})
+    const dataToSend = {buttonColor:getComputedStyle(popularbutton).backgroundColor,
+        buttonName:"Popular Anime",response:response}
+    const res = await chrome.tabs.sendMessage(tab.id,dataToSend)
     loading.style.display = "none"
     console.log(res)
     console.log(response)
--- scripts/content-script.js
+++ scripts/content-script.js
@@ -2,54 +2,69 @@ let dialog;
 let body = document.getElementsByTagName("body")[0];
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(dialog)
-    if(dialog){
-        console.log("present")
-        dialog.remove()
-    }
-    dialog = document.createElement('div');
-    dialog.style.zIndex = 20000
-    dialog.style.height = "250px"
-    dialog.style.width = "270px"
-    dialog.style.display = "block"
-    dialog.style.position = "fixed"
-    dialog.style.top = "50%"
-    dialog.style.left = "50%"
-    dialog.style.transform = "translate(-50%, -50%)"
-    dialog.style.backgroundColor = "white"
-    dialog.style.padding = "20px"
-    dialog.style.borderRadius = "10px"
-    dialog.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.3)"
-    dialog.style.opacity = 0
-    dialog.style.transition = "opacity 0.4s ease-in-out"
-    
-    let dialogHeader = document.createElement('div')
-    
-    dialogHeader.textContent = "Animex"
-    dialogHeader.style.backgroundColor = "lightgray"
-    dialogHeader.style.padding = "10px"
-    dialogHeader.style.fontSize = "20px"
-    dialogHeader.style.textAlign = "center"
-    dialogHeader.style.borderTopLeftRadius = "10px"
-    dialogHeader.style.borderTopRightRadius = "10px"
-    
-    dialog.appendChild(dialogHeader)
-    body.appendChild(dialog);
+        console.log(dialog)
+        if(dialog){
+            console.log("present")
+            dialog.remove()
+        }
+        dialog = document.createElement('div');
+        dialog.style.zIndex = 20000
+        dialog.style.minHeight = "20em"
+        dialog.style.minWidth = "25em"
+        dialog.style.maxWidth = "30em"
+        dialog.style.display = "block"
+        dialog.style.position = "fixed"
+        dialog.style.top = "50%"
+        dialog.style.left = "50%"
+        dialog.style.transform = "translate(-50%, -50%)"
+        dialog.style.backgroundColor = "white"
+        dialog.style.borderRadius = "10px"
+        dialog.style.boxShadow = "10px 10px 20px 0px rgba(0, 0, 207, 0.3)"
+        dialog.style.opacity = 0
+        dialog.style.transition = "opacity 0.4s ease-in-out"
+        
+        let dialogHeader = document.createElement('div')
+        
+        dialogHeader.textContent = request.buttonName
+        dialogHeader.style.backgroundColor = request.buttonColor
+        dialogHeader.style.color = "white"
+        dialogHeader.style.padding = "10px"
+        dialogHeader.style.fontSize = "20px"
+        dialogHeader.style.textAlign = "center"
+        dialogHeader.style.borderTopLeftRadius = "10px"
+        dialogHeader.style.borderTopRightRadius = "10px"
+        
+        dialog.appendChild(dialogHeader)
 
         console.log('request',request)
         let dialogContent = document.createElement('div')
         let list = document.createElement("ul")
+        list.style.padding = "0"
         request.response.forEach((ele,ind) => {
+            let para = document.createElement("p")
+            para.textContent = ele.animeTitle
+            para.style.color = "black"
+            para.style.fontSize = "15px"
+            para.style.margin = "0px"
             let listitem = document.createElement("li")
-            listitem.textContent = ele.animeTitle
+            listitem.style.color = "blue"
+            listitem.style.fontSize = "1.6em"
+            listitem.style.borderBottom = "3px solid blue";
+            listitem.style.padding = "0.6em 0 1em 0";
+            listitem.append(para)
             list.appendChild(listitem)
         });
 
-        dialogContent.style.height = "150px"
+        dialogContent.style.maxHeight = "21em"
         dialogContent.style.overflowY = "scroll"
         dialogContent.style.padding = "20px"
         dialogContent.appendChild(list)
         dialog.appendChild(dialogContent)
+
+        
+        body.appendChild(dialog);
+    
+        
 
         setTimeout(()=>{
             dialog.style.opacity = 1
